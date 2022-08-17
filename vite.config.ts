effbe2929714da18c8a1a3dfd006a2862c55fd85
Arrowana <8245419+Arrowana@users.noreply.github.com>
2022-08-17 22:56:39 +1000

    add back bits otherwise it breaks at runtime

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), inject({ Buffer: ["buffer", "Buffer"] })],
   build: {
     target: ["es2021"],
     sourcemap: true,
+  },
+  define: {
+    global: {},
   },
   optimizeDeps: {
     esbuildOptions: { target: "es2021" },
